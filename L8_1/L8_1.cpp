b1a0406365a03a1764a45985ffcdd1840871533b
davydenkomax <davydenko.3x3x3@gmail.com>
2023-12-12 23:08:26 +0700

    v2

--- L8_1/L8_1.cpp
+++ L8_1/L8_1.cpp
@@ -5,31 +5,6 @@
 #include <string>
 #include <cstdio>
 
-/**
- * @brief Проверяет строку на пустую
- * @param s проверяемая строка
- * @return false или true
-*/
-bool isEmpty(const std::string s) {
-    if (s[0] == '\0') return true;
-    bool flag;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == ' ') {
-            flag = true;
-        }
-        else {
-            flag = false;
-            break;
-        }
-    }
-    if (flag) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
 using namespace std;
 int main() {
     setlocale(LC_ALL, "ru");
@@ -39,11 +14,6 @@ int main() {
     string s;
     // Ввод строки в переменную s
     getline(cin, s);
-    // Проверка на пустую строку
-    if (isEmpty(s)) {
-        cerr << "Строка пустая";
-        return 1;
-    }
     // Вывод кодов элементов строки
     cout << "Код первого символа: " << (int)(s[0]) << endl;
     cout << "Код последнего символа: " << (int)(s[s.size() - 1]) << endl;
